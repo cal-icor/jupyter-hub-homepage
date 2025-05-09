aaadbf96a50009bcbf319895ed009bf2b67d4956
shane knapp <incomplete@gmail.com>
2025-05-09 13:33:36 -0700

    more testing for the hub homepage

--- extra-assets/js/login.js
+++ extra-assets/js/login.js
@@ -1,11 +1,11 @@
-//function redirectIfNeeded() {
-    // Only redirect utoronto.2i2c.cloud, lets us keep staging at staging.utoronto.2i2c.cloud
-//    if (window.location.hostname === 'utoronto.2i2c.cloud') {
+function redirectIfNeeded() {
+    // Only redirect jupyter.cal-icor.org, lets us keep staging at staging.jupyter.cal-icor.org
+    if (window.location.hostname === 'jupyter.cal-icor.org') {
         // Let's give users an indication that something is happening
-//        document.write("Redirecting you to jupyter.utoronto.ca");
-//        window.location.hostname = 'jupyter.utoronto.ca';
-//    }
-//}
+        document.write("Redirecting you to jupyter.cal-icor.org...");
+        window.location.hostname = 'jupyter.cal-icor.org';
+    }
+}
 function setInterface(interfaceUrl) {
     let loginUrl = new URL($('#home').data('authenticator-login-url'), document.location.origin);
     loginUrl.searchParams.set('next', '/hub/user-redirect/' + interfaceUrl)
@@ -15,10 +15,11 @@ function setInterface(interfaceUrl) {
     );
 }
 $(function() {
-    //redirectIfNeeded();
+    redirectIfNeeded();
     // if next query param is presentm just do nothing
     const nextPresent = new URL(document.location).searchParams.get('next');
-    if (!nextPresent) {
+    // /hub/ being next should be treated same as no next present
+    if (!nextPresent || nextPresent === "/hub/") {
         setInterface($("input[name='interface']:checked").val());
 
         $("input[name='interface']").change(function() {
